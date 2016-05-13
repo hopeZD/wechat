932d7147fb376094958d4be718d9c4b8eacd0a7a
hopeZ <zhengzuodi@gmail.com>
2016-05-13 15:16:34 +0800

    再次修改

--- index.php
+++ index.php
@@ -1,130 +1,35 @@
 <?php
-define('TOKEN','hope');
-$obj=new Weixin();
 
-if(!isset($_GET['echostr'])){
+    define("TOKEN", "hope");
+    $obj = new Weixin();
 
-    $obj->receive();
+    if(!isset($_GET['echostr'])) {
 
-}else{
-    $obj->checkSignature();
-}
+        //$obj->receive();
 
-
-class   Weixin{
-
-
-
-
-    public  function checkSignature()
-    {
-        $signature = $_GET["signature"];   //加密签名
-        $timestamp = $_GET["timestamp"]; //时间戳
-        $nonce = $_GET["nonce"];	//随机数
-
-        $token = TOKEN; //token
-
-
-        $tmpArr = array($token, $timestamp, $nonce);//组成新数组
-        sort($tmpArr, SORT_STRING);//重新排序
-        $tmpStr = implode( $tmpArr );//转换成字符串
-        $tmpStr = sha1( $tmpStr );  //再将字符串进行加密
-
-
-        if( $tmpStr == $signature ){
-
-            echo  $_GET['echostr'];
-        }else{
-            return false;
-        }
+    } else {
+        $obj->checkSignature();
     }
 
 
-    public  function  receive(){
+    class Weixin {
 
-        $obj= file_get_contents("php://input");
-        $postSql=simplexml_load_string($obj,'SimpleXMLElement',LIBXML_NOCDATA);
+        public function checkSignature() {
 
+            $signature = $_GET["signature"];
+            $timestamp = $_GET["timestamp"];
+            $nonce = $_GET["nonce"];
+            $token = TOKEN;
 
-        $this->logger("接受：\n".$obj);
+            $tmpArr = array($token, $timestamp, $nonce);
+            sort($tmpArr, SORT_STRING);
+            $tmpStr = implode($tmpArr);
+            $tmpStr = sha1($tmpStr);
 
-        if(!empty($postSql)){
-
-            switch(trim($postSql->MsgType)){
-
-                case "text" :
-                    $result=$this->receiveText($postSql);
-
-
-
-                    if(!empty($result)){
-                        echo $result;
-
-                    }else{
-
-                        $xml="<xml>
-						<ToUserName><![CDATA[%s]]></ToUserName>
-						<FromUserName><![CDATA[%s]]></FromUserName>
-						<CreateTime>%s</CreateTime>
-						<MsgType><![CDATA[%s]]></MsgType>
-						<Content><![CDATA[%s]]></Content>
-					  </xml>";
-
-
-                        echo $result=sprintf($xml,$postSql->FromUserName,$postSql->ToUserName,time(),$postSql->MsgType,"没有这条文本消息");
-
-                    }
+            if($tmpStr == $signature) {
+                return $_GET['echostr'];
+            } else {
+                return false;
             }
-
-
-
         }
-
-
-
     }
-
-
-    private function receiveText($postSql){
-        $content=trim($postSql->Content);
-
-
-        if(strstr($content,"你好")){
-            $xml="<xml>
-						<ToUserName><![CDATA[%s]]></ToUserName>
-						<FromUserName><![CDATA[%s]]></FromUserName>
-						<CreateTime>%s</CreateTime>
-						<MsgType><![CDATA[%s]]></MsgType>
-						<Content><![CDATA[%s]]></Content>
-					  </xml>";
-
-
-            $result=sprintf($xml,$postSql->FromUserName,$postSql->ToUserName,time(),$postSql->MsgType,"hello");
-
-            return $result;
-
-        }
-
-    }
-
-    private function logger($content){
-        $logSize=100000;
-
-        $log="log.txt";
-
-        if(file_exists($log) && filesize($log)  > $logSize){
-            unlink($log);
-        }
-
-        file_put_contents($log,date('H:i:s')." ".$content."\n",FILE_APPEND);
-
-    }
-
-}
-
-
-
-
-
-
-?>
