2bce3cb04e6cb6ee6afe0d64ed43c742a96994a4
hopeZ <zhengzuodi@gmail.com>
2016-05-17 10:33:07 +0800

    add curl

--- update.php
+++ update.php
@@ -5,5 +5,13 @@
  * Date: 16/5/17
  * Time: 上午10:26
  */
-$tpl = file_get_contents("http://www.baidu.com");
-echo $tpl;
+
+
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, 'http://news.baidu.com');
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+
+    curl_exec($ch);
+
+    curl_close($ch);
