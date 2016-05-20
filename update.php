1a97713f8662c04df8c18d7cb22f251e6fa1bfbe
hopeZ <zhengzuodi@gmail.com>
2016-05-20 17:28:40 +0800

    菜单

--- update.php
+++ update.php
@@ -46,41 +46,40 @@
 
     $token = get_token();
 
-    $url = " https://api.weixin.qq.com/cgi-bin/menu/create?access_token={$token}";
+    $url="https://api.weixin.qq.com/cgi-bin/menu/create?access_token={$token}";
 
-    $data = <<<php
-    {
-     "button":[
-     {	
-          "type":"click",
-          "name":"今日歌曲",
-          "key":"V1001_TODAY_MUSIC"
-      },
-      {
-           "name":"菜单",
-           "sub_button":[
-           {	
-               "type":"view",
-               "name":"搜索",
-               "url":"http://www.soso.com/"
-            },
-            {
-               "type":"view",
-               "name":"视频",
-               "url":"http://v.qq.com/"
-            },
-            {
-               "type":"click",
-               "name":"赞一下我们",
-               "key":"V1001_GOOD"
-            }]
-      }]
-    }
-
+    $data=<<<php
+        {
+         "button":[
+         {	
+              "type":"click",
+              "name":"今日歌曲",
+              "key":"V1001_TODAY_MUSIC"
+          },
+          {
+               "name":"菜单",
+               "sub_button":[
+               {	
+                   "type":"view",
+                   "name":"搜索",
+                   "url":"http://www.soso.com/"
+                },
+                {
+                   "type":"view",
+                   "name":"视频",
+                   "url":"http://v.qq.com/"
+                },
+                {
+                   "type":"click",
+                   "name":"赞一下我们",
+                   "key":"V1001_GOOD"
+                }]
+           }]
+     }
 php;
 
-    $result = http_curl($url, $data);
+    $result=http_curl($url,$data);
 
     var_dump($result);
-
+?>
 
