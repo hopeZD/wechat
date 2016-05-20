54c433afb732e57e80f324fe9faaabac763ebd1e
hopeZ <zhengzuodi@gmail.com>
2016-05-20 17:24:10 +0800

    自定义菜单

--- update.php
+++ update.php
@@ -45,14 +45,42 @@
     }
 
     $token = get_token();
-    //var_dump($token);
-    $type = "image";
-    $path = "hope/www/defualt/wechat/1.jpg";
-    $data = array("media" => "@".$path);
 
-    $url = "http://file.api.weixin.qq.com/cgi-bin/media/upload?access_token={$token}&type={$type}";
+    $url = " https://api.weixin.qq.com/cgi-bin/menu/create?access_token={$token}";
 
-    $arr = http_curl($url, $data);
+    $data = <<<php
+    {
+     "button":[
+     {	
+          "type":"click",
+          "name":"今日歌曲",
+          "key":"V1001_TODAY_MUSIC"
+      },
+      {
+           "name":"菜单",
+           "sub_button":[
+           {	
+               "type":"view",
+               "name":"搜索",
+               "url":"http://www.soso.com/"
+            },
+            {
+               "type":"view",
+               "name":"视频",
+               "url":"http://v.qq.com/"
+            },
+            {
+               "type":"click",
+               "name":"赞一下我们",
+               "key":"V1001_GOOD"
+            }]
+      }]
+    }
 
-    var_dump($arr);
+php;
+
+    $result = http_curl($url, $data);
+
+    var_dump($result);
+
 
